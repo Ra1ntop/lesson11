114287c5517bc2ae794fa6b4acc137c669519c2c
Ra1ntop <man111play@gmail.com>
2022-02-14 16:39:15 +0200

    first commit

---
+++ 1.py
@@ -0,0 +1,70 @@
+car = ['honda', 'lambarghini', 'volva']
+message = f"Я хочу машуну под маркой {car[1].title()}"
+print(message)
+
+
+car = ['honda', 'lambarghini', 'volva']
+message = f"Я хочу машуну под маркой {car[1].upper()}"
+print(message)
+
+
+car = ['honda', 'lambarghini', 'volva']
+print(car)
+car[2] = 'ducati'
+print(car)
+
+
+car = []
+print(car)
+car.append('honda')
+car.append('lambarghini')
+car.append('ducati')
+print(car)
+
+
+
+car = ['honda', 'lambarghini', 'volva']
+print(car)
+car.insert(1, 'ducati')
+print(car)
+
+
+
+car = ['honda', 'lambarghini', 'volva']
+print(car)
+
+del car[1]
+print(car)
+
+
+
+car = ['honda', 'lambarghini', 'volva']
+print(car)
+
+del_car = car.pop(0)
+print(car)
+print(del_car)
+
+
+
+user = ['u1', 'u2', 'u3', 'u4']
+user_v = []
+
+while user:
+    del_user = user.pop(o)
+    user_v.append(del_user)
+    v = f"{del_user} будет верефецирован на вашем сайте"
+print(user)
+print(user.v)
+
+
+
+car = ['honda', 'lambarghini', 'volva']
+
+car.sort(reverse=True)
+print(car)
+
+
+car = ['honda', 'lambarghini', 'volva']
+dLina = len(car)
+print(dLina)
